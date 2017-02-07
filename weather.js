2d68d5eb449921432296e8289096cea57f995fc8
Ryan Lane <ryan.lane@kellogg.northwestern.edu>
2017-02-06 23:16:27 -0600

    Added Skycons

--- weather.js
+++ weather.js
@@ -1,6 +1,9 @@
 $(function() {
 
 
+
+//  console.log(icons)
+
   var handleWeatherResponse = function(weather) {
     // "weather" is an object that holds all the information you need. Here, we
     // write it out to the console for easy viewing.
@@ -14,7 +17,7 @@ $(function() {
     // Let's get that data!
 
     var days = [];
-    var dayIcons = [];
+    var dayIcons = [0,0,0,0];
 
     for (i=0; i<5; i++) {
   //    console.log(Math.round(weather.daily.data[i+1].temperatureMax));
@@ -22,15 +25,6 @@ $(function() {
       days[i] = {month:datetemp.getMonth()+1,day:datetemp.getDate(),summary:weather.daily.data[i+1].summary,highTemp:Math.round(weather.daily.data[i+1].temperatureMax),lowTemp:Math.round(weather.daily.data[i+1].temperatureMin),icon:weather.daily.data[i].icon};
       console.log(days[i].icon);
 
-  /*    switch(days[i].icon) {
-        case "rain":
-          days[i].iconUrl=0;//"<img src="rain.png">";
-          break;
-        case default:
-          days[i].iconUrl=10;//"<img src="rain.png">";
-          break;
-        }
-      }*/
 	  }
 
 
@@ -66,26 +60,47 @@ $(function() {
     $('.weather-temp').html(CurTemp);
     $('.weather-hum').html(CurHum);
 
-    $('.icon-one').html(days[1].iconUrl);
+//    var temp = "<canvas id='" + days[1].icon + "' width='32' height='32'></canvas>"
+//    var temp = "<canvas id='clear-day' width='32' height='32'></canvas>"
+//    console.log(temp)
+
+    $('.icon-one').html("<canvas id='" + days[1].icon + "' width='32' height='32'></canvas>");
+//<canvas id="clear-day" width="32" height="32"></canvas>
     $('.date-one').html(dispDate1);
     $('.summary-one').html(dispDate1sum);
     $('.hightemp-one').html(dispDate1hightemp);
     $('.lowtemp-one').html(dispDate1lowtemp);
 
+    $('.icon-two').html("<canvas id='" + days[2].icon + "' width='32' height='32'></canvas>")
     $('.date-two').html(dispDate2);
     $('.summary-two').html(dispDate2sum);
     $('.hightemp-two').html(dispDate2hightemp);
     $('.lowtemp-two').html(dispDate2lowtemp);
 
+    $('.icon-three').html("<canvas id='" + days[3].icon + "' width='32' height='32'></canvas>")
     $('.date-three').html(dispDate3);
     $('.summary-three').html(dispDate3sum);
     $('.hightemp-three').html(dispDate3hightemp);
     $('.lowtemp-three').html(dispDate3lowtemp);
 
+    $('.icon-four').html("<canvas id='" + days[4].icon + "' width='32' height='32'></canvas>")
     $('.date-four').html(dispDate4);
     $('.summary-four').html(dispDate4sum);
     $('.hightemp-four').html(dispDate4hightemp);
     $('.lowtemp-four').html(dispDate4lowtemp);
+
+    var icons = new Skycons(),
+        list  = [
+          "clear-day", "clear-night", "partly-cloudy-day",
+          "partly-cloudy-night", "cloudy", "rain", "sleet", "snow", "wind",
+          "fog"
+        ],
+        i;
+    icons.color = "white"
+    for(i = list.length; i--; )
+      icons.set(list[i], list[i]);
+
+    icons.play();
   }
 
   // The "glue" that makes it all work. Don't really worry about this for now.
@@ -94,7 +109,7 @@ $(function() {
     //alert("pageload event fired!");
   event.preventDefault();
 
-
+//  var mylocation;
   $.getJSON('https://freegeoip.net/json/')
      .done (function(location)
      {
@@ -102,7 +117,9 @@ $(function() {
        var userLong = location.longitude;
        var city = location.city;
        var state = location.region_code;
+       mylocation = location.city;
        console.log(city,state);
+      // $('.locationInfo').html("Current location is " + city + ", " + state + ".")
        var newurl = "https://api.forecast.io/forecast/6dbe98374cc5b8f9ea63d5ec73de9c04/" + userLat + "," + userLong + "?callback=?";
 
        $.ajax({
@@ -111,8 +128,11 @@ $(function() {
          dataType: 'jsonp',
          contentType: "application/json",
          success: handleWeatherResponse
+
        });
 
      });
+
+//  console.log("This is my " + mylocation)
 //  });
 });
